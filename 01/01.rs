b570a8617ea698ae28cda261fe3fd2b7b6eeca8b
Rui Ramos <rui@ometria.com>
2019-12-03 11:14:31 +0000

    Simplify code for first rust problem

--- 01/01.rs
+++ 01/01.rs
@@ -3,50 +3,28 @@ use std::fs::File;
 use std::io;
 use std::io::prelude::*;
 
-fn fuel(m: usize) -> isize {
+fn fuel(m: &usize) -> isize {
     (m / 3 - 2) as isize
 }
 
-fn fuel_rec(m: usize) -> isize {
+fn fuel_rec(m: &usize) -> isize {
     let result = fuel(m);
     match result {
-        n if n > 0 => n + fuel_rec(n.try_into().unwrap()),
+        n if n > 0 => n + fuel_rec(&(n as usize)),
         _ => result,
     }
 }
 
-fn main() -> io::Result<()> {
-    let mut f = File::open("fuel.txt")?;
+fn main() {
+    let mut f = File::open("fuel.txt").unwrap();
     let mut buffer = String::new();
-    f.read_to_string(&mut buffer)?;
+    f.read_to_string(&mut buffer).unwrap();
 
-    let sum_of_mass: isize = buffer
-        .split('\n')
-        .map(|m| {
-            let value = m.parse::<usize>();
-            if let Ok(value) = value {
-                fuel(value)
-            } else {
-                0
-            }
-        })
-        .sum();
+    let input: Vec<usize> = buffer.lines().map(|m| m.parse().unwrap()).collect();
 
+    let sum_of_mass: isize = input.iter().map(fuel).sum();
     println!("01. {}", sum_of_mass);
 
-    let sum_of_mass: isize = buffer
-        .split('\n')
-        .map(|m| {
-            let value = m.parse::<usize>();
-            if let Ok(value) = value {
-                fuel_rec(value)
-            } else {
-                0
-            }
-        })
-        .sum();
-
+    let sum_of_mass: isize = input.iter().map(fuel_rec).sum();
     println!("02. {}", sum_of_mass);
-
-    Ok(())
 }
